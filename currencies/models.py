99d66aab8cdd7884b5eb9b539690d7bfc775f676
Ahmar Abdullah <ahmarabdullah@Ahmars-MacBook-Pro.local>
2021-05-12 11:28:11 +0530

    Removing the support for python2

--- currencies/models.py
+++ currencies/models.py
@@ -2,12 +2,12 @@
 
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from django.utils.encoding import python_2_unicode_compatible
+#from django.utils.encoding import python_2_unicode_compatible
 
 from .managers import CurrencyManager
 
 
-@python_2_unicode_compatible
+#@python_2_unicode_compatible
 class Currency(models.Model):
 
     code = models.CharField(_('code'), max_length=3,
